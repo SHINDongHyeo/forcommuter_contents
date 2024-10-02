0050285f4c6714b5c92e83455624447f378048f5
Donghyeok Shin <smdh1010@gmail.com>
2024-10-03 02:44:40 +0900

    feature: add GET contents/youtube

--- src/contents.controller.ts
+++ src/contents.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { ContentsService } from './contents.service';
+import YoutubeHot from './entities/youtube_hot.entitiy';
 
-@Controller()
+@Controller('contents')
 export class ContentsController {
 	constructor(private readonly contentsService: ContentsService) {}
 
-	@Get()
-	getHello(): string {
-		return this.contentsService.getHello();
+	@Get('youtube')
+	getAllYouTubeVideos(): Promise<YoutubeHot[]> {
+		return this.contentsService.getAllYouTubeVideos();
 	}
 }
--- src/contents.service.ts
+++ src/contents.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import YoutubeHot from './entities/youtube_hot.entitiy';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ContentsService {
-	getHello(): string {
-		return 'Hello Worlddsf!';
+	constructor(
+		@InjectRepository(YoutubeHot)
+		private readonly youTubeRepository: Repository<YoutubeHot>,
+	) {}
+
+	async getAllYouTubeVideos(): Promise<YoutubeHot[]> {
+		return await this.youTubeRepository.find({
+			take: 20,
+		});
 	}
 }
